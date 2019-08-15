e17e9c6ac96ccfa36c82c08444fbb187156a770a
xennyrains <53820384+xennyrains@users.noreply.github.com>
2019-08-15 11:27:09 +1000

    Build function improved

--- test.py
+++ test.py
@@ -1,27 +1,32 @@
 import re
 def same(item, target):
+  #print(item, target)
   return len([c for (c, t) in zip(item, target) if c == t])
 
+
 def build(pattern, words, seen, list):
+  #print(pattern)
   return [word for word in words
-                 if re.search(pattern, word) and word not in seen.keys() and
-                    word not in list]
+                 if re.match(pattern, word) and word not in seen.keys() and
+                    word if not list]
 
 def find(word, words, seen, target, path):
   list = []
   for i in range(len(word)):
     list += build(word[:i] + "." + word[i + 1:], words, seen, list)
+    #print(list)
   if len(list) == 0:
     return False
   list = sorted([(same(w, target), w) for w in list])
   for (match, item) in list:
     if match >= len(target) - 1:
       if match == len(target) - 1:
-        path.append(item)
+         path.append(item)
       return True
     seen[item] = True
   for (match, item) in list:
     path.append(item)
+    #print(match,item)
     if find(item, words, seen, target, path):
       return True
     path.pop()
