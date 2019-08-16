cd4fcb5c345913760c0838fb60d9ec2c8763e91a
JordanWardAllen <45648863+JordanWardAllen@users.noreply.github.com>
2019-08-17 00:31:54 +1000

    value input validation implemented

--- test.py
+++ test.py
@@ -1,60 +1,70 @@
 import re
 def same(item, target):
-  #print(item, target)
   return len([c for (c, t) in zip(item, target) if c == t])
 
-
 def build(pattern, words, seen, list):
-  #print(pattern)
   return [word for word in words
-                 if re.match(pattern, word) and word not in seen.keys() and
+                 if re.search(pattern, word) and word not in seen.keys() and
                     word if not list]
 
 def find(word, words, seen, target, path):
   list = []
   for i in range(len(word)):
     list += build(word[:i] + "." + word[i + 1:], words, seen, list)
-    #print(list)
   if len(list) == 0:
     return False
   list = sorted([(same(w, target), w) for w in list])
 
   for (match, item) in list:
-    if match >= len(target) - 1 and match == len(target) - 1:
-        if match < len(target):
-          path.append(item)
-        #use another list instead of path
+    if match >= len(target) -1:
+        path.append(item)
         return True
     seen[item] = True
+
   for (match, item) in list:
     path.append(item)
     if find(item, words, seen, target, path):
       return True
     path.pop()
 
-def initInput():
-  temp = True
-  while temp is True:
+
+def fileInput():
+  fileIsReal = False
+  while fileIsReal is False:
     initial = input("Enter dictionary name: ")
     try:
       file = open(initial)
-      temp = False
+      fileIsReal = True
       return file
     except FileNotFoundError:
       print("Not a file. Input a correct file")
 
 
-file = initInput()
+def valueInput():
+  inputCorrect = False
+  while inputCorrect is False:
+    start = input("Enter start word:")
+    target = input("Enter target word:")
+    if start.isalpha() == False or target.isalpha() == False and (len(start) == len(target)) is True:
+      print("needs alphabetical letters")
+    elif (len(start) == len(target)) is False:
+      print("Needs same length words")
+    else:
+      inputCorrect = True
+
+  return start , target
+
+
+file = fileInput()
 lines = file.readlines()
-start = input("Enter start word:")
+start, target = valueInput()
 words = []
 for line in lines:
   word = line.rstrip()
   if len(word) == len(start):
     words.append(word)
-target = input("Enter target word:")
 
-count = 0
+
 path = [start]
 seen = {start : True}
 if find(start, words, seen, target, path):
@@ -62,4 +72,3 @@ if find(start, words, seen, target, path):
   print(len(path) - 1, path)
 else:
   print("No path found")
-
